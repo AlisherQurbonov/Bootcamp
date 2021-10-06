d9ff4fa94397a25d1902f231516c408b849dc7f2
AlisherDe <aqurbonov865@gmail.com>
2021-10-07 02:59:29 +0500

    Lab2-Lab3-Lab4-Lab5

---
+++ Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using Bootcamp.Solutions;
+
+namespace Bootcamp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // var lab2 = new Lab2();
+            // lab2.Problem1();
+            // lab2.Problem2();
+            // lab2.Problem3();
+            // lab2.Problem4();
+            // lab2.Problem5();
+            // lab2.Problem7();
+            // lab2.Problem8();
+            // lab2.Problem9();
+            // var lab3 = new Lab3();
+            // lab3.Problem3_2();
+            // lab3.Problem3_3();
+            // lab3.Problem3_4();
+            // lab3.Problem3_2_2();
+            // lab3.Problem3_1();
+            // var lab4 = new lab4();
+            // lab4.Problem1();
+            // lab4.Problem2();
+            // lab4.Problem3();
+            // lab4.Problem4();
+            // lab4.Problem5();
+            // lab4.Problem6();
+            // lab4.Problem7();
+            // lab4.Problem8();
+            // lab4.Problem9();
+            // lab4.Problem10();
+            // var lab5 = new Lab5();
+            // lab5.Problem1();
+            // lab5.Problem2();
+            // lab5.Problem3();
+            // lab5.Problem4();
+            // lab5.Problem5();
+            // lab5.Problem6();
+            // lab5.Problem7();
+            // lab5.Problem8();
+            //   lab5.Problem9();
+            //   lab5.Problem10();
+            //    lab5.Problem11();
+            //    lab5.Problem12();
+            // lab5.Problem13();
+            // lab5.Problem14();
+            // lab5.Problem15();
+            // lab5.Problem16();
+            //    lab5.Problem17();
+            //  lab5.Problem18();
+            // lab5.Problem19();
+            // lab5.Problem20();
+            // lab5.Problem21();
+
+        }
+    }
+}
